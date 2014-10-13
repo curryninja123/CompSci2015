2e2a263d46cdd4eccdc09cba9dc82c9f59b31cce
Vishnu Iyer <vishnu72498@gmail.com>
2014-10-13 15:05:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
-}).listen(8124);
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
