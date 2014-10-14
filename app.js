c6d86594d3bf76c8b421716cc7314f6776af0473
Shreyas Balaji <shreyasb99@gmail.com>
2014-10-14 17:06:22 -0500

    Node setup

---
+++ app.js
@@ -0,0 +1,101 @@
+var express = require('express');
+var settings = require('./settings')
+var session = require('express-session')
+var MongoStore = require('connect-mongo')(session);
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// session setup
+app.use(session({
+    secret: settings.cookie_secret,
+    saveUninitialized: true,
+    resave: true,
+    store: new MongoStore({
+      db : settings.dbName,
+      host: settings.dbHost,
+      port: settings.dbPort
+    })
+}));
+
+// flash
+var flash = require('connect-flash');
+app.use(flash());
+app.use(function(req, res, next){
+    res.locals.success_messages = req.flash('success') || [];
+    res.locals.error_messages = req.flash('error') || [];
+    next();
+});
+
+userz = require('./models/userz.js');
+app.use(userz.loginvar());
+
+// controller setup
+app.use('/', routes);
+app.use('/users', users);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+var mongoose = require( 'mongoose' );
+var dbURI = settings.dbURI;
+mongoose.connect(dbURI);
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose default connection open to ' + dbURI);
+});
+mongoose.connection.on('error',function (err) {
+  console.log('Mongoose default connection error: ' + err);
+});
+
+
+app.listen(settings.port);
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
---
+++ settings.js
@@ -0,0 +1,6 @@
+module.exports.dbURI = 'mongodb://localhost/dhscompsci';
+module.exports.dbName = 'dhscompsci';
+module.exports.dbPort = 27017;
+module.exports.host = 'localhost';
+module.exports.cookie_secret = 'hsVwy4GLieLI90ZNlJD2heC3zRFWJjwHAeAnbxLvXcVzW18SelwogU0G0zcHd6IH';
+module.exports.port = 8000;
